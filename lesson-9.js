874c19702a7451c25dab5d703d59c4a945a7e15b
floatingnahid22 <nahidhossain>
2022-02-27 19:29:10 +0600

    lesson-9 completed

--- lesson-9.js
+++ lesson-9.js
@@ -1,6 +1,14 @@
-function User5() {
+function User5(email,name) {
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function(){
+        console.log(this.email, 'has logged in');
+    }
 
 }
 
-var userOne = new User5('ryu@ninjas.com', 'Ryu');
-var userTwo = new User5('yoshi@mariokorp.com', 'Yoshi');
+var userOne = new User5('nahid@gmail.com','Nahid');
+var userTwo = new User5('hossain@gmail.com','Hossain');
+console.log(userOne);
+userTwo.login();
