7d00c665cc4679833d9a6c6a15cfb74255c876a3
floatingnahid22 <nahidhossain>
2022-02-27 19:07:07 +0600

    lesson-8 complete

---
+++ lesson-8.js
@@ -0,0 +1,39 @@
+class User4 {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+}
+
+class Admin extends User4 {
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email
+        });
+    }
+}
+
+var userOne = new User4('ryu@ninjas.com', 'Ryu');
+var userTwo = new User4('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
+
+var users = [userOne, userTwo, admin];
+
+// admin.deleteUser(userTwo);
+userTwo.deleteUser(userOne); // won't work
+
+console.log(users);
