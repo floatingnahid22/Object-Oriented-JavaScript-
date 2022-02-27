0fe36d7dc342507c46e9c139f433f3f9496c7b76
floatingnahid22 <nahidhossain>
2022-02-27 17:03:09 +0600

    lesson-5 complete

---
+++ lesson-5.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(email,name){
+        this.email = email;
+        this.name = name;
+    }
+    
+}
+
+var userOne = new User('nahid@gmail.com','Nahid');
+var userTwo = new User('hossain@gmail.com','Hossain');
