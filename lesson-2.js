601db38fec5043157d0201684b597579a9c7d01f
floatingnahid22 <nahidhossain>
2022-02-27 16:07:27 +0600

    lesson-2 complete

--- lesson-2.js
+++ lesson-2.js
@@ -1,6 +1,12 @@
 var userOne = {
     email: 'nahid@gmail.com',
-    name: 'Nahid'
+    name: 'Nahid',
+    login(){
+        console.log(this.email, 'has logged in')
+    },
+    logout(){
+        console.log(this.email, 'has logged out')
+    },
 };
 
 console.log(userOne.name);
